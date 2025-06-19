8cc93f8335617b61c999d9c9ea9486833f00a7b5
Muhammad Fakhrul Amin <80448871+mfakhrulam@users.noreply.github.com>
2025-06-19 09:24:48 +0700

    Update ReqCreateProductDto.kt

--- src/main/kotlin/com/batch14/productmanagementservice/domain/dto/request/ReqCreateProductDto.kt
+++ src/main/kotlin/com/batch14/productmanagementservice/domain/dto/request/ReqCreateProductDto.kt
@@ -1,5 +1,6 @@
 package com.batch14.productmanagementservice.domain.dto.request
 
+import jakarta.validation.constraints.Min
 import jakarta.validation.constraints.NotBlank
 import jakarta.validation.constraints.NotNull
 
@@ -7,6 +8,7 @@ data class ReqCreateProductDto(
     @field:NotBlank(message = "Nama produk wajib diisi")
     val name: String,
     @field:NotNull(message = "Harga produk wajib diisi")
+    @field:Min(0, message = "Harga produk tidak boleh kurang dari 0")
     val price: Int,
     val description: String? = null,
     val isForSale: Boolean? = false,
